7e9605b23e101227ed08dff7538dc41155d00952
Meghna Das <gdyes128@gmail.com>
2023-10-30 11:04:02 +0530

    changes made

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import './app.css';
+import calculator from './components/calculator'
+
+function App() {
+  return (
+    <div className='app'>
+      <calculator />
+    </div>
+  );
+}
+
+export default App;
